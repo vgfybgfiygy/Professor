e66fde1c4628d57f7f62de4c03c96fa70c78d09e
Professor <118675414+SL-Auto-Filter-Bot@users.noreply.github.com>
2022-11-25 13:48:35 +0530

    Professor

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -51,7 +51,7 @@ async def save_group(bot, message):
                         await (temp.MELCOW['welcome']).delete()
                     except:
                         pass
-                temp.MELCOW['welcome'] = await message.reply_photo(photo=random.choice(PICS), caption=f"👋 Hello {u.mention},\n\nWelcome To <b>'{message.chat.title}'</b>")
+                temp.MELCOW['welcome'] = await message.reply_photo(photo=random.choice(PICS), caption=f"👋 Hello {u.mention},\n\nWelcome To <b>'{message.chat.title}'</b> Group.")
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
